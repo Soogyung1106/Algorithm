532deda68f4331d193c971ef5c70ce6d157d85f6
soogyung1106 <sally1106@naver.com>
2020-12-23 02:03:38 +0900

    '약수의 합' 문제풀이

---
+++ 프로그래머스 level1/약수의 합/약수의 합.cpp
@@ -0,0 +1,11 @@
+using namespace std;
+
+int solution(int n) {
+    int i = 1; int answer = 0;
+    while(i<=n){
+        if(n % i == 0) answer += i;
+        i++;
+    }
+    return answer;
+    
+}
