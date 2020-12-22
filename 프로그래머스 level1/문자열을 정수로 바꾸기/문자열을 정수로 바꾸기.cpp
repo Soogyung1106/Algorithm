c665a20a54f41481574b8d27107f5ae8e974ce5e
soogyung1106 <sally1106@naver.com>
2020-12-22 11:57:57 +0900

    '문자열을 정수로 바꾸기' 문제해결

---
+++ 프로그래머스 level1/문자열을 정수로 바꾸기/문자열을 정수로 바꾸기.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string s) {
+    /*
+    if(s[0] == '+'){
+        return stoi(s.substr(1, s.size()));
+    }else if(s[0] == '-'){
+        
+    }else{
+        return stoi(s);
+    }
+    */
+    return stoi(s);
+    
+}
