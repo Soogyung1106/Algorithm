6590c2fba3d075f1761f93a88b1f686e6b6504ed
soogyung1106 <sally1106@naver.com>
2020-12-28 18:20:30 +0900

    '짝지어 제거하기' 문제풀이

---
+++ 프로그래머스 level2/짝지어 제거하기/짝지어 제거하기.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int solution(string s)
+{
+    vector<int> v;
+    for(int i =0;i<s.size();i++){
+        if(!v.empty()){
+            if(v.back() == s[i]) v.pop_back();
+            else v.push_back(s[i]);
+        }else v.push_back(s[i]);
+    }
+
+    return v.empty() ? 1 : 0;
+}
