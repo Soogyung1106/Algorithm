6bb9623047cb9f3cf70ec9300e5fe29b0909ed73
soogyung1106 <sally1106@naver.com>
2021-01-02 15:17:59 +0900

    '문자열 내 마음대로 정렬하기' 복습

---
+++ 프로그래머스 level1/문자열 내 마음대로 정렬하기/복습.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int idx = 0;
+
+bool compare(string a, string b){
+
+    return a[idx] == b[idx] ? a <= b : a[idx] < b[idx];
+}
+
+vector<string> solution(vector<string> strings, int n) {
+    
+    idx = n;
+    sort(strings.begin(), strings.end(), compare);
+
+    return strings;
+}
